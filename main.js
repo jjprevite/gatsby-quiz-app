c724c34b02adc2f09ef7a3d53953394ffdcafb8e
Joe Previte <jjprevite@gmail.com>
2018-02-08 21:03:21 -0700

    -Change the structure of the quizQuestions to check for answer easier
    -write function to check quizAnswer

--- main.js
+++ main.js
@@ -3,66 +3,38 @@
 const quizQuestions = [
     {
         question: 'What is Gatsby.js?',
-        options: [
-            ['a framework', false],
-            ['a PWA (progressive web app) generator', false ],
-            ['a blazing-fast static site generator for React', false],
-            ['awesome + all of the above', true]
-        ]
+        options: ['a framework', 'a PWA (progressive web app) generator', 'a blazing-fast static site generator for React','awesome + all of the above'],
+        answer: 'awesome + all of the above',
     },
     {
         question: 'Gatsby.js uses the ___________ stack',
-        options: [
-            ['JAM (JavaScript, API, Markdown)', true],
-            ['MERN (MongoDB, Express.js, React.js, and Node.js)', false],
-            ['Ruby on Rails', false],
-            ['MEAN (MongoDB, Express.js, Angular.js, and Node.js)', false]
-        ]
+        options: ['JAM (JavaScript, API, Markdown)', 'MERN (MongoDB, Express.js, React.js, and Node.js)', 'Ruby on Rails', 'MEAN (MongoDB, Express.js, Angular.js, and Node.js)'],
+        answer: 'JAM (JavaScript, API, Markdown)'
     },
     {
         question: 'What year was Gatsby.js released? (Version 1.0.0)',
-        options: [
-            ['2010', false],
-            ['2017', true],
-            ['2011', false],
-            ['2014', false]
-        ]
+        options: ['2010', '2017', '2011','2014'],
+        answer: '2017'
     },
     {
         question: 'Gatsby.js uses what to load data from the server to a client?',
-        options: [
-            ['Falcor', false],
-            ['REST', false],
-            ['GraphQL', true],
-            ['all of the above', false]
-        ]
+        options: ['Falcor','REST','GraphQL','all of the above'],
+        answer: 'GraphQL'
     },
     {
         question: 'What makes Gatsby.js have a "faster time to interaction?',
-        options: [
-            ['progressive image loading', false],
-            ['responsive image loading', false],
-            ['inline CSS', false],
-            ['all of the above', true]
-        ]
+        options: ['progressive image loading', 'responsive image loading', 'inline CSS', 'all of the above'],
+        answer: 'all of the above'
     },
     {
         question: 'The equivalent to Gatsby.js for Ruby is...',
-        options: [
-            ['WordPress', false],
-            ['Drupal', false],
-            ['Jekyll', true],
-            ['Squarespace', false]
-        ]
+        options: ['WordPress', 'Drupal', 'Jekyll', 'Squarespace'],
+        answer: 'Jekyll'
     },
     {
         question: 'Gatsby.js is for...',
-        options: [
-            ['bloggers', false],
-            ['PWA fans', false],
-            ['React developers', false],
-            ['EVERYONE!', true]
-        ]
+        options: ['bloggers', 'PWA fans', 'React developers', 'EVERYONE!'],
+        answer: 'EVERYONE!'
     }
 ]
 
@@ -85,20 +57,20 @@ function generateQuestionElement(index) {
         <fieldset>
             <legend>Question ${questionNum+1}: ${quizQuestions[questionNum].question}</legend>
             <label>
-                <input type="radio" value="${quizQuestions[questionNum].options[0][0]}" name="answer" required>
-                <span>${quizQuestions[questionNum].options[0][0]}</span>
+                <input type="radio" value="${quizQuestions[questionNum].options[0]}" name="answer" required>
+                <span>${quizQuestions[questionNum].options[0]}</span>
             </label>
             <label>
-                <input type="radio" value="${quizQuestions[questionNum].options[1][0]}" name="answer" required>
-                <span>${quizQuestions[questionNum].options[1][0]}</span>
+                <input type="radio" value="${quizQuestions[questionNum].options[1]}" name="answer" required>
+                <span>${quizQuestions[questionNum].options[1]}</span>
             </label>
             <label>
-                <input type="radio" value="${quizQuestions[questionNum].options[2][0]}" name="answer" required>
-                <span>${quizQuestions[questionNum].options[2][0]}</span>
+                <input type="radio" value="${quizQuestions[questionNum].options[2]}" name="answer" required>
+                <span>${quizQuestions[questionNum].options[2]}</span>
             </label>
             <label>
-                <input type="radio" value="${quizQuestions[questionNum].options[3][0]}" name="answer" required>
-                <span>${quizQuestions[questionNum].options[3][0]}</span>
+                <input type="radio" value="${quizQuestions[questionNum].options[3]}" name="answer" required>
+                <span>${quizQuestions[questionNum].options[3]}</span>
             </label>
             <button type="submit" class="js-submit-question">Submit</button>
         </fieldset>`;
@@ -115,14 +87,21 @@ function handleAnswerSubmitted() {
         console.log('question answer submitted..');
         let answer = $("input[name='answer']:checked").val();
         console.log(answer);
+        checkQuestionAnswer(answer);
     });
     //3. check it based on questionCount
     //4. if it's right, display something
     //5. if it's wrong, display another thing
 }
 
-function checkQuestionAnswer() {
-    //write this cool function
+function checkQuestionAnswer(answer) {
+    //V fix that so it's not hard coded...
+    const questionArrNum = 0;
+    if(answer == quizQuestions[questionArrNum].answer) {
+        console.log('correct answer!');
+    } else {
+        console.log('wrong answer :(');
+    }
 }
     
 
